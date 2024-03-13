5d972b66f2c94b08f4ec4ae3bbd8ffc7da854611
Harsh2542 <147285211+Harsh2542@users.noreply.github.com>
2024-03-13 01:07:21 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages
 import streamlit as st
+pip install snowflake-connector-python
 from snowflake.snowpark.functions import col
 cnx = st.connection("snowflake")
 session = cnx.session()
